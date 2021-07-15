7b7f0f1f8f71a489980f40a8e2546c1e020a125b
Matthew Gammon <mgammon91@gmail.com>
2021-07-15 00:09:16 -0400

    updated to display a jumbotron 404 message with a link back to the home page

---
+++ src/Layout/NotFound.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function NotFound() {
+  return (
+    <div>
+      <div className="NotFound">
+        <div className="jumbotron bg-light text-dark shadow-lg">
+          <h1 className="display-2">404 Not Found!</h1>
+          <p className="lead font-weight-bold">
+            The page you have requested does not exist. Fear not! Simply click
+            the button below to navigate to the home page.
+          </p>
+          <p>Happy Studying!</p>
+          <Link
+            to="/"
+            className="btn btn-primary btn-lg"
+            href="#"
+            role="button"
+          >
+            <span className="oi oi-home p-1" />
+            Go Home
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
